9997a6cdb36ec933b0929e9e678991635693730c
Astryxion <s.pergolotti@my.ccsu.edu>
2026-04-08 01:40:25 -0400

    Add files via upload

---
+++ src/client/java/com/astryxion/backtools/client/core/ClientModBusEvents.java
@@ -0,0 +1,40 @@
+package com.astryxion.backtools.client.core;
+
+import com.astryxion.backtools.client.render.BackToolLayer;
+import com.astryxion.backtools.common.BackTools;
+import net.fabricmc.fabric.api.client.rendering.v1.LivingEntityRenderLayerRegistrationCallback;
+import net.minecraft.client.renderer.entity.player.AvatarRenderer;
+import net.minecraft.world.entity.EntityType;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.WeakHashMap;
+
+public final class ClientModBusEvents
+{
+	private static final Set<AvatarRenderer<?>> LAYERED = Collections.newSetFromMap(new WeakHashMap<>());
+
+	private ClientModBusEvents()
+	{
+	}
+
+	public static void registerPlayerLayer()
+	{
+		LivingEntityRenderLayerRegistrationCallback.EVENT.register((entityType, entityRenderer, registrationHelper, context) -> {
+			if (entityType == EntityType.PLAYER && entityRenderer instanceof AvatarRenderer<?> avatarRenderer && LAYERED.add(avatarRenderer))
+			{
+				registrationHelper.register(new BackToolLayer(avatarRenderer));
+				BackTools.LOGGER.info("Back Tools: attached BackToolLayer to an AvatarRenderer instance");
+			}
+		});
+	}
+
+	public static void onConfigReloaded()
+	{
+		net.minecraft.client.Minecraft mc = net.minecraft.client.Minecraft.getInstance();
+		if (mc != null)
+		{
+			mc.execute(EventHandler::setupConfig);
+		}
+	}
+}
